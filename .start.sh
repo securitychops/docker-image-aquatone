caaa27a918c3661d975161ba45bd2b783e8eb3e8
Security Chops <39027035+securitychops@users.noreply.github.com>
2019-10-13 15:34:38 -0400

    Create .start.sh

---
+++ .start.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export PATH=$PATH:/root/go/bin
+
+mkdir -p ~/.aws/
+echo "[default]" >> ~/.aws/credentials
+echo "aws_access_key_id = $S3_BUCKET_KEY" >> ~/.aws/credentials
+echo "aws_secret_access_key = $S3_BUCKET_SECRET" >> ~/.aws/credentials
+
+current_time=$(date "+%Y.%m.%d.%H.%M.%S")
+
+aws s3 cp s3://$S3_BUCKET_NAME/tmp/$DOMAINS_FILE /tmp/scanme
+
+cat /tmp/scanme | aquatone -out /tmp/output -ports xlarge -resolution "640,480" -save-body 
+
+mkdir -p /tmp/$TLD/$current_time
+
+mv -v /tmp/output/* /tmp/$TLD/$current_time
+
+aws s3 cp /tmp/$TLD/* s3://$S3_BUCKET_NAME/reports/$TLD --recursive
