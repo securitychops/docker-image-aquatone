a688f5006ac3ef5cf9424275f8fe4a7aca98c062
Security Chops <39027035+securitychops@users.noreply.github.com>
2019-10-14 12:48:58 -0400

    fixing timeout issue

--- .start.sh
+++ .start.sh
@@ -11,7 +11,7 @@ current_time=$(date "+%Y.%m.%d.%H.%M.%S")
 
 aws s3 cp s3://$S3_BUCKET_NAME/tmp/$DOMAINS_FILE /tmp/scanme
 
-cat /tmp/scanme | aquatone -out /tmp/output -ports xlarge -resolution "640,480" -save-body 
+cat /tmp/scanme | aquatone -out /tmp/output -ports xlarge -resolution "640,480" -scan-timeout 500
 
 mkdir -p /tmp/$TLD/$current_time
 
